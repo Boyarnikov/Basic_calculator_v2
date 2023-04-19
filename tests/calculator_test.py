c58a20d27147f84abe80e242bbfe7660864bd8ce
Boyar <you@example.com>
2023-04-19 20:47:11 +0300

    Delete calculator_test.py raise test

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -7,11 +7,6 @@ class CalculatorTest(TestCase):
     def test_add(self):
         self.assertEqual(calculator.add(10, 15), 25)
 
-    def test_add_with_strings(self):
-        with self.assertRaises(ValueError) as e:
-            calculator.add("10", 15)
-        self.assertEqual("Expected two integers", e.exception.args[0])
-
 
 if __name__ == '__main__':
     main()
