e0b48688cc81ad5aede8d9c9adc8535773abf8ed
Boyar <you@example.com>
2023-04-19 20:17:37 +0300

    Add "add" logic to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,2 +1,6 @@
-def info_name():
-    print(__name__)
+def add(numb1, numb2):
+    if type(numb1) != int or type(numb2) != int:
+        raise ValueError("Expected two integers")
+    summ = numb1 + numb2
+    return summ
+
